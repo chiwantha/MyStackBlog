c0abb6c1d275e87b02d04aa8a89f7426313da9ae
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-10-28 22:42:57 +0530

    free commit

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://192.168.8.100:8800/server",
+  baseURL: "http://192.168.8.101:8800/server",
   withCredentials: true,
 });
